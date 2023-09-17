c517ee16edcf91aafb44777217a9dcd040cd6a21
fallen-ecstasy <imfrost245@gmail.com>
2023-09-17 18:13:30 +0530

    17sep

--- src/util/parser.js
+++ src/util/parser.js
@@ -1,7 +1,7 @@
 import csvToJson from 'convert-csv-to-json';
 import fs from 'fs';
 
-let fileInputName = '16sep.csv'
+let fileInputName = '17sep.csv'
 let fileOutputName = 'data.json'
 
 const res = csvToJson.fieldDelimiter(',').supportQuotedField(true).getJsonFromCsv(fileInputName);
